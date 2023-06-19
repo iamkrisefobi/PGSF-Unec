480507e2f121097a49fc1e7f321d2a7ba6e29da5
iamkrisefobi <krisfobbby@gamil.com>
2023-06-19 02:08:07 +0100

    Add swiperjs for testimonial slides & add callout

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
+// for collapsibles
 const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+
+// link to swiper js
+var swiper = new Swiper(".testimonial__slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  loop:true,
+});
