173dae32708f2f9ecc9a3a3ca048e1fc65512074
iamkrisefobi <krisfobbby@gamil.com>
2023-06-29 00:18:30 +0100

    Modify the events section & add count down timer

--- js/main.js
+++ js/main.js
@@ -21,6 +21,62 @@ var swiper = new Swiper(".testimonial__slider", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+    disabledClass: 'swiper-button-disabled',
+    hideOnClick: true,
   },
   loop: true,
 });
+
+
+// Sendforth event countdown timer (event1)
+let countDownDate1 = new Date("Jul 09, 2023 14:00:00").getTime();
+let a = setInterval(function () {
+  let currentTime = new Date().getTime();
+  let timeDifference = countDownDate1 - currentTime;
+
+  let days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+  document.querySelector("#event1__days").innerHTML = days;
+  document.querySelector("#event1__hours").innerHTML = hours;
+  document.querySelector("#event1__minutes").innerHTML = minutes;
+  document.querySelector("#event1__seconds").innerHTML = seconds;
+
+  if(timeDifference < 0){
+    clearInterval(a);
+    document.querySelector("#event1__days").innerHTML = "00";
+    document.querySelector("#event1__hours").innerHTML = "00";
+    document.querySelector("#event1__minutes").innerHTML = "00";
+    document.querySelector("#event1__seconds").innerHTML = "00";
+  }
+
+}, 1000)
+
+
+// Christmas event countdown timer (event4)
+let countDownDate4 = new Date("Dec 9, 2023 14:00:00").getTime();
+let d = setInterval(function () {
+  let currentTime = new Date().getTime();
+  let timeDifference = countDownDate4 - currentTime;
+
+  let days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+  document.querySelector("#event4__days").innerHTML = days;
+  document.querySelector("#event4__hours").innerHTML = hours;
+  document.querySelector("#event4__minutes").innerHTML = minutes;
+  document.querySelector("#event4__seconds").innerHTML = seconds;
+
+  if(timeDifference < 0){
+    clearInterval(d);
+    document.querySelector("#event4__days").innerHTML = "00";
+    document.querySelector("#event4__hours").innerHTML = "00";
+    document.querySelector("#event4__minutes").innerHTML = "00";
+    document.querySelector("#event4__seconds").innerHTML = "00";
+  }
+
+},1000)
